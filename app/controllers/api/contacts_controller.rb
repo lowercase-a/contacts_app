b1a86cbbdc6744b2da45a4b0540cc77e07ea67d8
Brian <riceb53@gmail.com>
2019-03-06 19:53:01 -0600

    make middle name and bio, update rest of app

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -6,11 +6,13 @@ class Api::ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(
-                           first_name: params[:first_name],
-                           last_name: params[:last_name],
-                           email: params[:email],
-                           phone_number: params[:phone_number]
-                          )
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      bio: params[:bio],
+      middle_name: params[:middle_name]
+    )
     @contact.save
     render 'show.json.jbuilder'
   end
@@ -27,6 +29,8 @@ class Api::ContactsController < ApplicationController
     @contact.last_name = params[:last_name] || @contact.last_name
     @contact.email = params[:email] || @contact.email
     @contact.phone_number = params[:phone_number] || @contact.phone_number
+    @contact.bio = params[:bio] || @contact.bio
+    @contact.middle_name = params[:middle_name] || @contact.middle_name
 
     @contact.save
     render 'show.json.jbuilder'
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ApplicationRecord
+  def full_name
+    "#{first_name} #{middle_name} #{last_name}"
+    # first_name + " " + last_name
+  end
 end
